007a1b0b85ae4fc34a358e4b43c26eec5753c7e0
Vitalii Tron <virtustron@gmail.com>
2021-11-11 09:57:37 +0200

    Add project files.

---
+++ DataGridViewWPF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGridViewWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PhonesContainer phones;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            phones = new PhonesContainer();
+            //phonesGrid.ItemsSource = phones;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Phone simplePhone;
+            simplePhone = new Phone();
+
+            simplePhone.Company = "Nokia";
+            simplePhone.Title = "6200";
+            simplePhone.Price = 100;
+
+
+            //Phone phone = new Phone { Title = "iPhone 6S", Company = "Apple", Price = 54990 };
+
+
+
+            phones.Add("iPhone 6S", "Apple", 54990);
+            phones.Add(simplePhone);
+
+            //phonesGrid.Items.Refresh();
+            //phonesGrid.ItemsSource = null;
+            //phonesGrid.ItemsSource = phones;
+        }
+
+
+        
+
+
+    }
+}
---
+++ DataGridViewWPF/PhonesContainer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridViewWPF
+{
+    class PhonesContainer
+    {
+        List<Phone> items;
+
+        public PhonesContainer()
+        {
+            items = new List<Phone>();
+        }
+
+
+        public void Add(string Title, string Company, int Price)
+        {
+            Phone PhoneToAdd = new Phone { Title = Title, Company = Company, Price = Price };
+
+            items.Add(PhoneToAdd);
+        }
+
+        public void Add(Phone PhoneToAdd)
+        {
+            items.Add(PhoneToAdd);
+        }
+
+    }
+}
